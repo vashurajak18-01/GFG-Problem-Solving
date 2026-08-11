613f94076da9a01da8f05c20e8a35ffc0b7a82a9
Vashu Rajak <vashurajak660@gmail.com>
2026-08-11 17:32:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.py
+++ Difficulty: Easy/Selection Sort/selection-sort.py
@@ -1,16 +1,14 @@
-class Solution: 
-    def selectionSort(self, arr):
-        # code here
-        n = len(arr)
-        
-        for i in range(0, n):
+class Solution:
+    def selectionSort(self, nums):
+        n = len(nums)
+
+        for i in range(n):
             min_idx = i
-            
-            for j in range(i+1, n):
-                
-                if arr[j] < arr[min_idx] :
-                    min_idx = j 
-                    
-            arr[i], arr[min_idx] = arr[min_idx], arr[i]
-            
-        return arr
+
+            for j in range(i + 1, n):
+                if nums[j] < nums[min_idx]:
+                    min_idx = j
+
+            nums[i], nums[min_idx] = nums[min_idx], nums[i]
+
+        return nums
