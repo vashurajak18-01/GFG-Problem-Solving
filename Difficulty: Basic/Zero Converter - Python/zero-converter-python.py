2c75651ae5bc6ba46cf0bef3d37280a4d3ed038b
Vashu Rajak <vashurajak660@gmail.com>
2026-08-24 21:20:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Zero Converter - Python/zero-converter-python.py
+++ Difficulty: Basic/Zero Converter - Python/zero-converter-python.py
@@ -1,12 +1,14 @@
 def pos(n):
+    ## Write the code
     n -= 1
-    while n >= 0:
-        print(n, end=' ')
+    while n>=0:
+        print(n,end=' ')
         n-=1
         
     
 def neg(n):
+    ##Write the code
+    while n<=0:
+        print(n,end=' ')
+        n+=1
     
-    while n <= 0:
-        print(n, end=' ')
-        n += 1
