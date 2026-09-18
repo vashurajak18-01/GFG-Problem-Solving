5a06f2835b20e63a9b5f32833a64d6ad37a53562
Vashu Rajak <vashurajak660@gmail.com>
2026-09-18 19:28:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.py
+++ Difficulty: Easy/Height of Binary Tree/height-of-binary-tree.py
@@ -6,15 +6,42 @@ class Node:
         self.right = None
 '''
 
+# class Solution:
+#     def height(self, root):
+        # code here
+        # def solve(node):
+        #     if node == None:
+        #         return -1
+        #     left_height = solve(node.left)
+        #     right_height = solve(node.right)
+
+        #     return 1 + max(left_height, right_height)
+
+        # return solve(root)
+
+
 class Solution:
     def height(self, root):
-        # code here
-        def solve(node):
-            if node == None:
-                return -1
-            left_height = solve(node.left)
-            right_height = solve(node.right)
 
-            return 1 + max(left_height, right_height)
+        if root is None:
+            return -1
+        
+        queue = deque()
+        height = 0
+        
+        queue.append(root)
 
-        return solve(root)
+        while len(queue) != 0:
+            level_size = len(queue)
+            height += 1
+            
+            for _ in range(level_size):
+                e = queue.popleft()
+                
+                if e.left is not None:
+                    queue.append(e.left)
+                    
+                if e.right is not None:
+                    queue.append(e.right)
+                    
+        return height - 1
